d232154770fce69158a5426b61e52693990a5f0d
komatineni leela venkata sai lokesh <lokeshklvs@gmail.com>
2026-03-11 21:13:56 +0530

    Specify template folder for Flask app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import io
 import base64
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
